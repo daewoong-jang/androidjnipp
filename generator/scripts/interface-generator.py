e67c3553ce021a9d895f776801fa82abaa5611a0
daewoong.jang <daewoong44@aol.com>
2017-03-11 15:27:48 +0900

    Update license block.

--- generator/scripts/interface-generator.py
+++ generator/scripts/interface-generator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2
 
-# Copyright (C) 2015 Naver Labs. All rights reserved.
+# Copyright (C) 2015 NAVER Labs. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
